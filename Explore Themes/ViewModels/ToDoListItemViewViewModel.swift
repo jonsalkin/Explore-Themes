f52f3acdb2da75213cb0ea50dc1ffa752758cd13
jonsalkin <50082962+jonsalkin@users.noreply.github.com>
2023-06-14 10:23:23 -0400

    Fetch user profile from firebase. Fetch stored to do list for the signed in user. Set up logout function. Fixed viewModel in ToDoListItemView to be: ToDoListItemViewViewModel().

--- Explore Themes/ViewModels/ToDoListItemViewViewModel.swift
+++ Explore Themes/ViewModels/ToDoListItemViewViewModel.swift
@@ -4,11 +4,28 @@
 //
 //  Created by Jon Salkin on 6/11/23.
 //
-
+import FirebaseAuth
+import FirebaseFirestore
 import Foundation
 
 // ViewModel for a single to do list item view (each row in items list)
 // Primary tab
 class ToDoListItemViewViewModel: ObservableObject {
     init() {}
+    
+    func toggleIsDone(item: ToDoListItem) {
+        var itemCopy = item
+        itemCopy.setDone(!item.isDone)
+        
+        guard let uid = Auth.auth().currentUser?.uid else {
+            return
+        }
+        
+        let db = Firestore.firestore()
+        db.collection("users")
+            .document(uid)
+            .collection("todos")
+            .document(itemCopy.id)
+            .setData(itemCopy.asDictionary())
+    }
 }
